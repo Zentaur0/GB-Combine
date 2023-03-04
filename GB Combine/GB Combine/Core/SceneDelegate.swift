073800aa7988ced45450472b01c3cb9de1608a56
AntonSVTSV <freshbreathe@gmail.com>
2023-03-04 15:13:33 +0700

    Add Main screen

--- GB Combine/GB Combine/Core/SceneDelegate.swift
+++ GB Combine/GB Combine/Core/SceneDelegate.swift
@@ -20,7 +20,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         window = UIWindow(windowScene: scene)
         window?.makeKeyAndVisible()
-        window?.rootViewController = ViewController()
+        window?.rootViewController = MainViewController()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
