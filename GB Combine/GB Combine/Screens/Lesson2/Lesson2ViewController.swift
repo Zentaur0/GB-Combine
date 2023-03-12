69a1625b373fdfa4770338c8e42375ebd6cdf0e9
AntonSVTSV <freshbreathe@gmail.com>
2023-03-12 17:00:25 +0700

    Add Leson2ViewController

---
+++ GB Combine/GB Combine/Screens/Lesson2/Lesson2ViewController.swift
@@ -0,0 +1,12 @@
+//
+//  Lesson2ViewController.swift
+//  GB Combine
+//
+//  Created by Антон Сивцов on 12.03.2023.
+//
+
+import UIKit
+
+final class Lesson2ViewController: UIViewController {
+    
+}
